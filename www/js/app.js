87dbb82e916e0d6761fca575044d7dc2f726883e
emreiser <emilie.reiser@gmail.com>
2016-04-12 07:43:36 -0400

    Add config file and default headers to requests

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,52 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services', 'sosConfig'], function($httpProvider, SOS_CONFIG){
+    $httpProvider.defaults.headers.common['Authorization'] = SOS_CONFIG.accessKey;
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+  /**
+   * The workhorse; converts an object to x-www-form-urlencoded serialization.
+   * @param {Object} obj
+   * @return {String}
+   */ 
+
+  var param = function(obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+      
+    for(name in obj) {
+      value = obj[name];
+        
+      if(value instanceof Array) {
+        for(i=0; i<value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+      
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
+ 
+  // Override $http service's default transformRequest
+  $httpProvider.defaults.transformRequest = [function(data) {
+    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+  }];
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
---
+++ www/js/config.js
@@ -0,0 +1,5 @@
+angular.module('sosConfig', [])
+.constant('SOS_CONFIG', {
+  accessKey: "483498d29536829d348f879e226f1388",
+  aggregatorUrl: "http://dev.aggregate.promisetracker.org/",
+});
